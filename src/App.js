371a54700b4c4204568496760609b733db0d72d7
HarringtonR <ross.ludeke@gmail.com>
2018-10-28 22:04:45 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  <div>
+    <div className="pos-f-t">
+      <div className="collapse" id="navbarToggleExternalContent">
+        <div className="bg-dark p-4">
+          <h5 className="text-white h4">Collapsed content</h5>
+          <span className="text-muted">Toggleable via the navbar brand.</span>
+        </div>
       </div>
+      <nav className="navbar navbar-dark bg-dark">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </nav>
+    </div>
+    <div >
+      <main role="main" className = 'main'>
+        <h1 class="cover-heading">Ross Ludeke</h1>
+        <p class="lead"> Full Stack Web Developer, Ex Navy Rescue Swimmer, Racing Enthusiast </p>
+        <p class="lead">
+          <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+        </p>
+      </main>
+    </div>
+    <div className ='projects'>
+        <div className="card">
+          <img className="card-img-top" src="https://imgur.com/L5I5zBC.jpg" alt="Cardcap"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      <div className="card">
+          <img className="card-img-top" src="https://imgur.com/L5I5zBC.jpg" alt="Cardcap"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      <div className="card">
+          <img className="card-img-top" src="https://imgur.com/L5I5zBC.jpg" alt="Cardcap"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+
+
+    </div>
+    <footer>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Navbar w/ text</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Features</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Pricing</a>
+            </li>
+          </ul>
+          <span className="navbar-text">
+            Navbar text with an inline element
+          </span>
+        </div>
+      </nav>
+    </footer>
+ </div>
+
     );
   }
 }
