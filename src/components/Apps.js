596d54281511797e5dbee2929b4f7ad4844b2004
HarringtonR <ross.ludeke@gmail.com>
2018-11-14 19:48:14 -0500

    layout

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -5,9 +5,8 @@
        return (
         <div className ='apps'>
             <div className ='row'>
-               <div className='box'>
-                <h2>IMG</h2>
-               </div>
+               <div className = 'boxImage'>
+                </div>
               <div className='box'>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet dolore reprehenderit eos fugit quasi, inventore! Totam sapiente fuga dolorem laboriosam ducimus excepturi, magnam quisquam obcaecati provident, consectetur dignissimos ad sed.</p>
               </div>
@@ -16,14 +15,12 @@
               <div className='box'>
                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet dolore reprehenderit eos fugit quasi, inventore! Totam sapiente fuga dolorem laboriosam ducimus excepturi, magnam quisquam obcaecati provident, consectetur dignissimos ad sed.</p>
                </div>
-               <div className='box'>
-                <h2>IMG</h2>
-             </div>
+                  <div className = 'boxImage2'>
+                </div>
           </div>
           <div className ='row'>
-               <div className='box'>
-                <h2>IMG</h2>
-               </div>
+                  <div className = 'boxImage3'>
+                </div>
               <div className='box'>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet dolore reprehenderit eos fugit quasi, inventore! Totam sapiente fuga dolorem laboriosam ducimus excepturi, magnam quisquam obcaecati provident, consectetur dignissimos ad sed.</p>
               </div>
