c21e35f71a6ae5939431e2a3228801fefebfd196
HarringtonR <ross.ludeke@gmail.com>
2018-12-10 17:14:42 -0500

    online test

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -4,33 +4,55 @@
      render() {
        return (
         <div className ='apps' id='section2'>
-          <h2 className='apptitle'> work</h2>
+          <h2 className='apptitle'> apps</h2>
           <img src='https://imgur.com/JfGQgE9.png' alt ='arrow'/>
            <div className='container'>
              <div className='box'>
               <div className='images p1'>
                </div>
-               <p> www.findtrackdays.com</p>
-               <p> Website was buit to help people find tracks and events to go racing at</p>
+               <div className='text'>
+                 <h4> www.findtrackdays.com</h4>
+                 <p> FindTrackDays is a react application being buit as a simple go to resource for people find tracks and events to go racing.</p>
+               </div>
              </div>
              <div className='box'>
                <div className='images p2'>
+               </div>
+               <div className='text'>
+                 <h4> News&Chat</h4>
+                 <p>React application built to combine google news api and pushers chatkit so people have a new way of discussing current events. </p>
                </div>
              </div>
              <div className='box'>
               <div className='images  p3'>
                </div>
+               <div className='text'>
+                 <h4> Instagram Remake</h4>
+                 <p> A group project in react to rebuild as much of instagram for fun.</p>
+               </div>
              </div>
              <div className='box'>
               <div className='images  p4'>
+               </div>
+               <div className='text'>
+                 <h4> ChatRoulette</h4>
+                 <p> I rebuilt ChatRoulette in React using SimpleWebRTC with a timer on the conversation for an added feature.</p>
                </div>
              </div>
              <div className='box'>
               <div className='images  p5'>
                </div>
+               <div className='text'>
+                 <h4> The Replicant</h4>
+                 <p> Chose your own story game built using JS  </p>
+               </div>
              </div>
              <div className='box'>
               <div className='images p6'>
+               </div>
+               <div className='text'>
+                 <h4> React-Native Weather App</h4>
+                 <p> Simple react-native application giving both todays forcast and the 5 day.</p>
                </div>
              </div>
            </div>
--- src/components/Bio.js
+++ src/components/Bio.js
@@ -5,6 +5,7 @@ import '../css/bio.css'
     return (
        <div className ='bio' id='section3'>
          <h2>bio</h2>
+           <img className ='arrow' src='https://imgur.com/JfGQgE9.png' alt ='arrow'/>
            <div className='personal'>
              <img src='https://imgur.com/SUJ6qRC.jpg' alt='me' />
              <p className='info'>Hi! Im Ross, A former US Navy Rescue Swimmer now Full Stack Web Developer. I make apps in react + react native and race motorcycles. I genuinely seek to work and live creatively pushing the scope of my experiences and hand on heart say I love what I do.</p>
@@ -26,8 +27,6 @@ import '../css/bio.css'
            <div className='skill css'>
            </div>
             <div className='skill post'>
-           </div>
-           <div className='skill html'>
            </div>
          </div>
 
